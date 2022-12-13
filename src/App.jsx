c059d2c4d11f6cb416f3ddc0ca2a528c9ada72dd
RaquibDox <70raquib@gmail.com>
2022-12-13 09:34:57 +0530

    the Router is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.scss'
+import MovieDetail from './components/MovieDetail/MovieDetail'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-    </div>
+      <Router>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/movie/:imdbID" element={<MovieDetail />} />
+            <Route path="*" element={<PageNotFound />}/>
+        </Routes>
+        <Footer />
+      </Router>
   )
 }
 
