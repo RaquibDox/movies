8c32d3fe31a24cbe92c0390fa68cbe9225149948
RaquibDox <70raquib@gmail.com>
2022-12-17 13:15:29 +0530

    Worked on the MovieListing Component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+
+    const {data} = props;
+    console.log("data: ", data);
+
     return (
-        <div>
-            MovieCard
+        <div className='card-item'>
+            <div className="card-inner">
+                <div className="card-top">
+                    <img src={data.Poster} alt={data.Title} />
+                </div>
+                <div className="card-bottom">
+                    <div className="card-info">
+                        <h4>{data.Title}</h4>
+                        <p>{data.Year}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { getAllMovies } from '../../features/movies/movieSlice';
+import { useSelector } from 'react-redux';
+import MovieCard from '../MovieCard/MovieCard';
+import "./MovieListing.scss";
 
 const MovieListing = () => {
+    // const movies = useSelector((state) => state.movies.movies);
+    const movies = useSelector(getAllMovies);
+    // console.log(movies);
+
+    let renderMovies = "";
+
+   renderMovies = movies.Response === "True" ?(
+        movies.Search.map((movie, index) =>{
+            return <MovieCard key={index} data={movie} />
+        })
+   ):(
+        <div className="movies-error">
+            <h2>{movies.error}</h2>
+        </div>
+   );
+
+   console.log(renderMovies);
+
+
     return (
-        <div>
-            MovieListing
+        <div className='movie-wrapper'>
+            <div className="movie-list">
+                <h2>Movies1</h2>
+                <div className="movie-container">
+                    {renderMovies}
+                </div>
+            </div>
         </div>
     );
 };
